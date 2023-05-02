c51dcc95c3eaea567dab9af30ffc9ba746439752
Nodir <kebridon.nemat.life@gmail.com>
2023-05-03 01:47:01 +0500

    Initial commit

---
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherForecast.kt
@@ -0,0 +1,46 @@
+package com.plcoding.weatherapp.presentation
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun WeatherForecast(
+    state: WeatherState,
+    modifier: Modifier = Modifier,
+    dayIndex: Int
+) {
+    state.weatherInfo?.weatherDataPerDay?.get(dayIndex)?.let { data ->
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = "0${dayIndex+3}/05",
+                fontSize = 20.sp,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyRow(content = {
+                items(data) { weatherData ->
+                    HourlyWeatherDisplay(
+                        weatherData = weatherData,
+                        modifier = Modifier
+                            .height(100.dp)
+                            .padding(horizontal = 16.dp)
+                    )
+                }
+            })
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
